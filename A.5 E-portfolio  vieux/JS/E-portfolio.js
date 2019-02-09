fcaf7962b856f6d563ffbf9dfe9bac5c486c8d6a
RosarNicolas2TL1 <he201426@students.ephec.be>
2019-02-09 21:33:49 +0100

    suppression dossier

--- A.5 E-portfolio  vieux/JS/E-portfolio.js
+++
@@ -1,6 +0,0 @@
-﻿/* JS E-portfolio*/
-function changeColor(){
-    var tab=['white','#ecf0f1','#bdc3c7','#999'];
-    var y = document.getElementById('colorx').value;
-    document.body.style.backgroundColor = tab[y];
-}
